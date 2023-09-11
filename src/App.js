63af15504ee1c05b1e6143001aa9114f77884481
Philip Gyamfi <philab.pg@gmail.com>
2023-09-11 11:18:24 +0000

    useState hook practicals

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
-
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
 
   return (
     <div className="App">
       {showEmployees ?  (
         <>
+        <input type='text' onChange={(e) => { console.log(e.target.value); setRole(e.target.value)}} />
         <Employee name="Caleb" role="Intern" />
-        <Employee name="Aby" />
+        <Employee name="Aby" role={role} />
         <Employee name="John" />
 
         </>
